59816d9503703d3cc59cfdac29c50be26f285197
santiago holguin <santiholguing@gmail.com>
2022-04-01 10:19:36 -0500

    proyect creation and calculator

---
+++ main.py
@@ -0,0 +1,13 @@
+from Calculadora import *
+menuCalculadora()
+#calculadora(1,2,2)
+
+# def Celsius(kelvin):
+#    assert(kelvin>= -273.15),"esta malo, lo maximo que puede llegar es a 0"
+# #    return (kelvin-273)
+#
+# print(Celsius(-273.16))
+#
+#
+#
+# assert ((calculadora(3, 3, 3)) == 9),"resultado no esperado"
